594e505bb5bb66b59a680f79a1c2b96adb13b1df
Qinhai <qxia45@gmail.com>
2026-01-20 19:53:38 -0500

    Secure API keys and update config

--- src/App.jsx
+++ src/App.jsx
@@ -173,7 +173,7 @@ const TRANSLATIONS = {
   zh: {
     menu: {
       home: "首頁",
-      appointments: "線上的預約",
+      appointments: "線上預約",
       services: "服務項目",
       whatWeTreat: "治疗范围",
       about: "關於我們",
